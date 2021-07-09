0f29a9de678cca2a6be070d1da2e5e3863debdfd
Hiroki Furuichi <hirokicodes@gmail.com>
2021-07-10 07:49:12 +0900

    trying without redis options object

--- src/cache/redisClient.ts
+++ src/cache/redisClient.ts
@@ -1,13 +1,17 @@
 import redis from "redis";
 
-export const redisClient =
-  process.env.NODE_ENV === "production"
-    ? redis.createClient({
-        host: process.env.REDIS_URL!,
-        port: parseInt(process.env.REDIS_PORT!),
-        password: process.env.REDIS_PASSWORD!,
-      })
-    : redis.createClient();
+// export const redisClient =
+//   process.env.NODE_ENV === "production"
+//     ? redis.createClient({
+//         host: process.env.REDIS_URL!,
+//         port: parseInt(process.env.REDIS_PORT!),
+//         password: process.env.REDIS_PASSWORD!,
+//       })
+//     : redis.createClient();
+
+export const redisClient = process.env.REDIS_URL
+  ? redis.createClient(process.env.REDIS_URL)
+  : redis.createClient();
 
 redisClient.on("error", function (error) {
   console.error(error);
